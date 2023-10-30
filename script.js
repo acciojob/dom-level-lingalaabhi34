431b8b6743762116b87e99df72f298b11c5c1acc
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-10-30 23:25:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",function(){
+    let element = document.getElementById("level");
+    let domlevel=1;
+    while(element && element!==document.documentElement){
+        element = element.parentElement;
+        domlevel++;
+    }
+    alert(`The level of the element is:${domlevel}`);
+})
